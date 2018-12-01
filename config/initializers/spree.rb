5258b447d3c6cf2824fab6a645f415311d639e65
John Chen <john@linkedinpedia.com>
2018-12-01 10:57:35 +0800

    Changed admin backend logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -31,7 +31,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
+  config.admin_interface_logo = "logo/urbanrad-logo.svg"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
